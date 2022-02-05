a2d4cac6f1c5874f5967d6c33a0ed197029f0438
Julien <minecarman53@protonmail.com>
2022-02-05 15:19:34 -0500

    Added suport for downloading files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+var request = require('request');
+var fs = require('fs');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
@@ -45,3 +47,56 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+//File downloading function
+/* USEAGE -->
+downloadFile({
+  remoteFile: "https://someurl/foo.bar",
+  localFile: `${__dirname}/tmp/foo.bar`,
+  onProgress: function (received,total){
+      var percentage = Math.trunc((received * 100) / total);
+      console.log(percentage + "% | " + received + " bytes out of " + total + " bytes.");
+  }
+}).then(function(){
+  console.log("File succesfully downloaded");
+});
+*/
+function downloadFile(configuration){
+  return new Promise(function(resolve, reject){
+      // Save variable to know progress
+      var received_bytes = 0;
+      var total_bytes = 0;
+
+      var req = request({
+          method: 'GET',
+          uri: configuration.remoteFile
+      });
+
+      var out = fs.createWriteStream(configuration.localFile);
+      req.pipe(out);
+
+      req.on('response', function ( data ) {
+          // Change the total bytes value to get progress later.
+          total_bytes = parseInt(data.headers['content-length' ]);
+      });
+
+      // Get progress if callback exists
+      if(configuration.hasOwnProperty("onProgress")){
+          req.on('data', function(chunk) {
+              // Update the received bytes
+              received_bytes += chunk.length;
+
+              configuration.onProgress(received_bytes, total_bytes);
+          });
+      }else{
+          req.on('data', function(chunk) {
+              // Update the received bytes
+              received_bytes += chunk.length;
+          });
+      }
+
+      req.on('end', function() {
+          resolve();
+      });
+  });
+}
